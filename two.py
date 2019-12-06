b765429318f5d717f7923272d8d91f07d3e3beeb
Derek Liu <derek@derekliu.me>
2019-12-06 12:28:41 -0500

    modified solution to day 2

--- two.py
+++ two.py
@@ -24,15 +24,15 @@ def parttwo(input_list, noun, verb):
     def grouper(iterable, n, fillvalue=None):
         args = [iter(iterable)] * n
         return zip_longest(*args, fillvalue=fillvalue)
-    input_data[1] = noun
-    input_data[2] = verb
-    for i in grouper(input_data, 4):
+    input_list[1] = noun
+    input_list[2] = verb
+    for i in grouper(input_list, 4):
         if i[0] == 1:
-            input_data[i[3]] = input_data[i[1]] + input_data[i[2]]
+            input_list[i[3]] = input_list[i[1]] + input_list[i[2]]
         elif i[0] == 2:
-            input_data[i[3]] = input_data[i[1]] * input_data[i[2]]
+            input_list[i[3]] = input_list[i[1]] * input_list[i[2]]
         elif i[0] == 99:
-            return input_data[0], noun, verb
+            return input_list[0], noun, verb
 
 from itertools import product
 from copy import deepcopy
@@ -40,5 +40,5 @@ for i, j in product([i for i in range(0, 100)], repeat=2):
     list_copy = deepcopy(input_data)
     output, noun, verb = parttwo(input_list=list_copy, noun=i, verb=j)
     if output == 19690720:
-        print("the noun is {} and verb is {}".format(noun, verb))
+        print("result: {}".format(100 * noun + verb))
         break
